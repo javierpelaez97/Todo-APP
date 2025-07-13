ac386ad9c0c6d627f7439685572e0361a6d2aa12
Javi <javierpelaezgimenez@gmail.com>
2025-07-13 21:48:56 +0200

    Modificadas las variables de entorno

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,42 @@
-
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Navegador from './components/navegador'
-import Home from './pages/home'
-import Tareas from './pages/tareas';
-import Login from './pages/login';
-import Register from './pages/register';
-import { useContext,  } from 'react';
-import { SessionContext } from './contexts/SessionContext';
-require('dotenv').config();
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navegador from "./components/navegador";
+import Home from "./pages/home";
+import Tareas from "./pages/tareas";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { useContext } from "react";
+import { SessionContext } from "./contexts/SessionContext";
 
 
 function App() {
-
-
-  const {user} = useContext(SessionContext)
-
-  
+  const { user } = useContext(SessionContext);
 
   console.log("APP", user);
 
-  
-  
-
   return (
     <>
-    
-    
       <header>
         <Navegador></Navegador>
-
       </header>
       <main>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
-          <Route path='/login' element={user ? <Navigate to="/misTareas"></Navigate>:<Login></Login>}></Route>
-          <Route path='/misTareas' element={<Tareas usuario={user}></Tareas>}></Route>
-          <Route path='/register' element={<Register></Register>}></Route>
+          <Route
+            path="/login"
+            element={
+              user ? <Navigate to="/misTareas"></Navigate> : <Login></Login>
+            }
+          ></Route>
+          <Route
+            path="/misTareas"
+            element={<Tareas usuario={user}></Tareas>}
+          ></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
         </Routes>
-
-      
-        
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/tareas.jsx
+++ frontend/src/pages/tareas.jsx
@@ -1,48 +1,36 @@
-
 import { useContext, useEffect, useState } from "react";
 import Tarea from "../components/tarea";
 import CrearTarea from "../components/CrearTarea";
 import axios from "axios";
+require('dotenv').config();
 
+export default function Tareas({ usuario }) {
+  const [tareas, setTareas] = useState([]);
 
+  const fetchTareas = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/tasks/userTask`
+      );
+      setTareas(response.data);
+    } catch (error) {
+      console.error("Error al traer las tareas", error);
+    }
+  };
 
-export default function Tareas ({usuario}){
+  useEffect(() => {
+    fetchTareas();
+  }, []);
 
-    const [tareas,setTareas] = useState([])
-    
+  return (
+    <>
+      <h1>Todas mis tareas</h1>
+      <CrearTarea user={usuario} onTraerTareas={fetchTareas}></CrearTarea>
+      {tareas.map((tarea) => (
+        <Tarea user={usuario} tareas={tarea}></Tarea>
+      ))}
 
-    
-        const fetchTareas = async () => {
-            
-            try{
-                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/tasks/userTask`)
-                setTareas(response.data)
-            }catch(error){
-                console.error('Error al traer las tareas',error)
-            }
-        }
-        
-        useEffect (()=>{
-            fetchTareas()
-        },[])
-    
-    
-    
-
-    return (
-        <>
-
-           <h1>Todas mis tareas</h1>
-                <CrearTarea user = {usuario} onTraerTareas= {fetchTareas}></CrearTarea>
-                {tareas.map(tarea =>(
-                    <Tarea user = {usuario}  tareas = {tarea}  ></Tarea>
-                     
-                ))
-                }
-            
-            <h2>Tareas borradas</h2>
-
-        </>
-        
-    )
+      <h2>Tareas borradas</h2>
+    </>
+  );
 }
